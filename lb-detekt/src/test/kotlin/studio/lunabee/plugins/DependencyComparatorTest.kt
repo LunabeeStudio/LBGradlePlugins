a439d409395004e01fbb7969b5609b40e32cd4ab
rlatapy-luna <remi.latapy@lunabee.com>
2025-01-20 12:41:41 +0100

    Add multiline block

--- lb-detekt/src/test/kotlin/studio/lunabee/plugins/DependencyComparatorTest.kt
+++ lb-detekt/src/test/kotlin/studio/lunabee/plugins/DependencyComparatorTest.kt
@@ -2,6 +2,7 @@ package studio.lunabee.plugins
 
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
 class DependencyComparatorTest {
 
@@ -21,5 +22,32 @@ class DependencyComparatorTest {
         val actual = expected.shuffled().sortedWith(comparator)
 
         assertContentEquals(expected, actual)
+    }
+
+    @Test
+    fun multiline_block_test() {
+        val input = listOf(
+            "implementation(libs.aaa)",
+            "implementation(libs.ccc) {\n    exclude(libs.mmm)\n}",
+            "if(condition) {\n    api(libs.vvv)\n} else {\n    api(libs.bbb)\n}",
+            "api(uuu)",
+        )
+        val actual = input.shuffled().sortedWith(comparator).joinToString("\n")
+
+        val expected = """
+            implementation(libs.aaa)
+            implementation(libs.ccc) {
+                exclude(libs.mmm)
+            }
+            api(uuu)
+            if(condition) {
+                api(libs.vvv)
+            } else {
+                api(libs.bbb)
+            }
+        """.trimIndent()
+
+
+        assertEquals(expected, actual, actual)
     }
 }
