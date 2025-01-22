1d40455ae918e98b2a6ed434075343e772a83775
rlatapy-luna <remi.latapy@lunabee.com>
2025-01-22 09:50:33 +0100

    Make lbdetekt inherit from detekt
    Remove configure/init

--- plugins/lb-detekt/src/main/kotlin/studio/lunabee/plugins/LBDetektPlugin.kt
+++ plugins/lb-detekt/src/main/kotlin/studio/lunabee/plugins/LBDetektPlugin.kt
@@ -2,8 +2,11 @@
 
 package studio.lunabee.plugins
 
+import io.gitlab.arturbosch.detekt.DetektPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.register
 
 /**
  * This plugin initializes and configures the Detekt plugin. Generally, we recommend applying this plugin directly in the
@@ -21,8 +24,29 @@ import org.gradle.api.Project
  */
 class LBDetektPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.extensions.create("lbDetekt", LBDetektExtension::class.java)
-        target.applyPlugin("detekt")
+        // Register sort dependencies scripts.
+        registerSortDependencies(target)
+        registerSortLibs(target)
+
+        target.extensions.findByType(LBDetektExtension::class.java) ?: target.extensions.create(
+            "lbDetekt",
+            LBDetektExtension::class.java,
+        )
+        target.pluginManager.apply(DetektPlugin::class.java)
         target.dependencies.add("detektPlugins", target.buildDependency("detektFormatting"))
+    }
+
+    private fun registerSortDependencies(project: Project): TaskProvider<SortBuildDependenciesTask> {
+        return project.tasks.register<SortBuildDependenciesTask>("sortBuildDependencies") {
+            group = "formatting"
+            description = "Sorts dependencies and plugins alphabetically in all build.gradle.kts files throughout the project."
+        }
+    }
+
+    private fun registerSortLibs(project: Project): TaskProvider<SortLibsVersionsTomlTask> {
+        return project.tasks.register<SortLibsVersionsTomlTask>("sortLibsVersionsToml") {
+            group = "formatting"
+            description = "Sorts the libraries and versions in libs.versions.toml alphabetically"
+        }
     }
 }
