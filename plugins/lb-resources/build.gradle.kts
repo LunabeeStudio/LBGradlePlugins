8687df2f8bc90f4c05fc3c2a0499e1e5e5ca7d32
rlatapy-luna <remi.latapy@lunabee.com>
2025-02-21 09:52:50 +0100

    bump 1.0.0 🎉

--- plugins/lb-resources/build.gradle.kts
+++ plugins/lb-resources/build.gradle.kts
@@ -11,7 +11,7 @@ gradlePlugin {
         create("studio.lunabee.plugins.resources") {
             id = "studio.lunabee.plugins.resources"
             implementationClass = "studio.lunabee.plugins.LBResourcesPlugin"
-            version = "0.9.1"
+            version = "1.0.0"
             displayName = "LBResources"
             description = "This plugin allows you to configure a task to download resources from any provider."
             tags = listOf("android", "lunabee", "resources")
