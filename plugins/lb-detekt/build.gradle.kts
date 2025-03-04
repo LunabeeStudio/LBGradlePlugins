2f4e194565fe443ca3afb42cbeb33663d8b885d9
rlatapy-luna <remi.latapy@lunabee.com>
2025-03-04 10:51:04 +0100

    Exclude .idea from detekt

--- plugins/lb-detekt/build.gradle.kts
+++ plugins/lb-detekt/build.gradle.kts
@@ -17,7 +17,7 @@ gradlePlugin {
         create("studio.lunabee.plugins.detekt") {
             id = "studio.lunabee.plugins.detekt"
             implementationClass = "studio.lunabee.plugins.LBDetektPlugin"
-            version = "1.0.0"
+            version = "1.0.1"
             displayName = "LBDetekt"
             description = "This plugin allows you to configure Detekt to ensure consistent code style across all projects."
             tags = listOf("detekt", "android", "lunabee", "code", "style")
