4369a28fb916e76fc481a68247efb2231be4d71d
Rémi Latapy <45555889+rlatapy-luna@users.noreply.github.com>
2026-06-18 09:06:34 +0200

    Update LBDetekt plugin version to 2.2.1

--- plugins/lb-detekt/build.gradle.kts
+++ plugins/lb-detekt/build.gradle.kts
@@ -37,7 +37,7 @@ gradlePlugin {
         create("studio.lunabee.plugin.detekt") {
             id = "studio.lunabee.plugin.detekt"
             implementationClass = "studio.lunabee.plugins.LBDetektPlugin"
-            version = "2.2.0"
+            version = "2.2.1"
             displayName = "LBDetekt"
             description = project.description
             tags = listOf("detekt", "android", "lunabee", "code", "style")
